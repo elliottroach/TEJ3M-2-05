c3f8be477a05f499d1b9e00446d80df16146a8cd
elliottroach <elliott.roach@stu.ocsb.ca>
2026-03-10 14:45:03 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
 """
 Made by Elliott Roach
-Made on 2026 feb
-this turns the light on and off
+Made on 2026 Mar
+this turns a servo 0 to 90
 """
 
 import board
 import digitalio
 import time
+from adafruit_motor import servo
 
 # setup
-pin5 = digitalio.DigitalInOut(board.GP5)
-pin5.direction = digitalio.Direction.OUTPUT
+data_wire = pwmio.PWMOut(board.gp2, frequency=50)
+
+my_servo = servo.ContinuousServo(data_wire)
+my_servo.angle = 0
 
 while True:
-    pin5.value = True
+    my_servo.angle = 0
     time.sleep(1)
-    pin5.value = False
+    my_servo.angle = 90
     time.sleep(1)
     
