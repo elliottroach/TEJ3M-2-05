114a167046abcaefa8d1f92b13859bd4564b37f7
elliottroach <elliott.roach@stu.ocsb.ca>
2026-03-23 14:43:53 -0400

    Fix formatting and whitespace in code.py

--- code.py
+++ code.py
@@ -10,11 +10,12 @@ import pwmio
 from adafruit_motor import servo
 
 # setup
-pwm = pwmio.PWMOut(board.GP2, duty_cycle=2 ** 15, frequency=50)
+pwm = pwmio.PWMOut(board.GP2, duty_cycle= 2 ** 15, frequency= 50)
 
 my_servo = servo.Servo(pwm)
 my_servo.angle = 0
 
+# doing actions
 while True:
     my_servo.angle = 0
     time.sleep(1)
